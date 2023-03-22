e1a07c0131a910c46ca282ab08af13bffb0dd643
Phuwanesh <76715327+Phux78@users.noreply.github.com>
2023-03-22 14:47:12 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -14,4 +14,4 @@ def test_write_main():
     name="Phuwanesh"
     response = client.post("/callname", json={"name": name})
     assert response.status_code == 200
-    assert response.json() == {'hello': name}
+    assert response.json() == {"hello": name}
