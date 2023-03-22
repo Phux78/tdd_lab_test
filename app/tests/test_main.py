779ede865012a8e8d4c9388657836ee6f102c0c9
Phuwanesh <76715327+Phux78@users.noreply.github.com>
2023-03-22 14:41:53 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "Worlddd"}
+
+def test_write_main():
+    name="Phuwanesh"
+    response = client.post("/callname", json={"name", name})
+    assert response.status_code == 200
+    assert response.json() == {"hello": name}
